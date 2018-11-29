fdb4a1b8062b4ce5c97c267dd3011cb8109dc639
Himanshu Satwick <himanshusatwick60@gmail.com>
2018-11-28 19:22:07 -0500

    uploading firebase and angular test proj

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyAt_WVD2XKc9S51j350r3U5TVNxWEWySwA",
-    authDomain: "fs1prod-d8486.firebaseapp.com",
-    databaseURL: "https://fs1prod-d8486.firebaseio.com",
-    projectId: "fs1prod-d8486",
-    storageBucket: "fs1prod-d8486.appspot.com",
-    messagingSenderId: "575238110626"
-  }
+  production: false
 };
 
 /*
